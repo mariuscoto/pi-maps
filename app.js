af711327e9c2d4ebb479aa83cf5f4619cf459bec
Marius Cotofana <marius.coto@gmail.com>
2016-01-20 07:57:53 +0200

    Use  if available

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App started!');
 });
